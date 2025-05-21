62a313f2d03504016cc57f5d7ce17613ac622f16
krokosik <wikkra@gmail.com>
2025-05-21 23:28:57 +0200

    Project page proper responsive layout

--- src/ui/components/ui/carousel.tsx
+++ src/ui/components/ui/carousel.tsx
@@ -178,7 +178,7 @@ const CarouselContent = React.forwardRef<
 	const { carouselRef, orientation } = useCarousel()
 
 	return (
-		<div ref={carouselRef} className="overflow-hidden">
+		<div ref={carouselRef} className="size-full overflow-hidden">
 			<div
 				ref={ref}
 				className={cn(
--- src/ui/modules/Project.tsx
+++ src/ui/modules/Project.tsx
@@ -61,7 +61,7 @@ export default function Project({
 					/>
 				</div>
 				<div className="col-span-full grid-cols-subgrid grid-rows-3 gap-x-6 px-4 md:grid md:pt-0 xl:gap-x-0">
-					<div className="row-span-3 grid size-full grid-rows-subgrid text-5xl font-medium md:text-6xl xl:col-span-12 xl:col-start-8">
+					<div className="row-span-3 grid size-full grid-rows-subgrid text-5xl font-medium md:text-6xl xl:col-span-12 xl:col-start-7">
 						<span className="self-end" style={{ color: colors.accentColor }}>
 							//:
 						</span>
@@ -75,7 +75,7 @@ export default function Project({
 							</span>
 						</h3>
 					</div>
-					<div className="col-start-2 row-span-2 row-start-2 pt-8 md:pt-0 xl:col-span-12 xl:col-start-[20]">
+					<div className="col-start-2 row-span-2 row-start-2 pt-8 md:pt-0 xl:col-span-13 xl:col-start-[19]">
 						<Pretitle
 							className="px-4 text-xl xl:text-2xl"
 							style={{ color: colors.accentColor }}
@@ -84,32 +84,33 @@ export default function Project({
 						</Pretitle>
 					</div>
 				</div>
-				<div className="col-span-full row-span-2 h-full grid-cols-subgrid md:col-start-1 md:row-start-1 md:grid md:divide-x md:divide-black">
-					<div className="hidden xl:col-span-7 xl:block" />
-					<div className="flex h-full items-center p-2 pt-8 xl:col-span-12 xl:pt-2">
-						<Carousel opts={{ loop: true }}>
-							<CarouselContent className="items-center">
-								{gallery?.images.map((image) => (
-									<CarouselItem
-										key={image._key}
-										// className={clsx(image._type !== 'youtube')}
-									>
-										<figure className="relative max-h-[500px] w-full">
-											{image._type === 'youtube' ? (
-												<YouTubePlayer url={image.url} width="100%" />
-											) : (
-												<Img className="mx-auto object-contain" image={image} />
-											)}
-										</figure>
-									</CarouselItem>
-								))}
-							</CarouselContent>
-							<CarouselPrevious className="left-0 z-10 bg-inherit backdrop-blur-md backdrop-brightness-125 hover:bg-current" />
-							<CarouselNext className="right-0 z-10 bg-inherit backdrop-blur-md hover:bg-current" />
-							<CarouselDots />
-						</Carousel>
-					</div>
-					<div className="size-full px-4 py-16 xl:col-span-12 xl:px-4 xl:py-0">
+				<div className="col-span-full row-span-2 h-full grid-cols-subgrid grid-rows-1 md:col-start-1 md:row-start-1 md:grid md:divide-x md:divide-black">
+					<div className="hidden xl:col-span-6 xl:block" />
+					<Carousel
+						opts={{ loop: true }}
+						className="h-full p-2 pt-8 xl:col-span-12 xl:pt-2"
+					>
+						<CarouselContent className="h-full">
+							{gallery?.images.map((image) => (
+								<CarouselItem key={image._key} className="h-full">
+									<figure className="relative flex size-full max-h-[500px] items-center">
+										{image._type === 'youtube' ? (
+											<YouTubePlayer url={image.url} width="100%" />
+										) : (
+											<Img
+												className="mx-auto h-full object-contain"
+												image={image}
+											/>
+										)}
+									</figure>
+								</CarouselItem>
+							))}
+						</CarouselContent>
+						<CarouselPrevious className="left-0 z-10 bg-inherit backdrop-blur-md backdrop-brightness-125 hover:bg-current" />
+						<CarouselNext className="right-0 z-10 bg-inherit backdrop-blur-md hover:bg-current" />
+						<CarouselDots />
+					</Carousel>
+					<div className="size-full px-4 py-16 xl:col-span-13 xl:px-4 xl:py-0">
 						<div className="3xl:text-4xl flex size-full flex-col justify-center text-2xl font-normal sm:text-3xl">
 							<CustomPortableText value={description} />
 						</div>
